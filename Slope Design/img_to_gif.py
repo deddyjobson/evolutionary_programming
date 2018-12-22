773a7b7fe76624098c2be696810abbc6fab19eda
Deddy Jobson <dedjob@hotmail.com>
2018-12-23 05:07:02 +0530

    saves trouble of manually deleting pngs later

--- Slope Design/img_to_gif.py
+++ Slope Design/img_to_gif.py
@@ -4,7 +4,8 @@ import os
 
 filenames = glob(os.path.join('Images','*.png'))
 
-with imageio.get_writer('movie.gif', mode='I', duration=0.1) as writer:
+with imageio.get_writer('movie.gif', mode='I', duration=0.2) as writer:
     for filename in filenames:
         image = imageio.imread(filename)
         writer.append_data(image)
+        os.remove(filename)
